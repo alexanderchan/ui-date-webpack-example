a4b1b4678ed516f7b7272b58f9c4b282e4894479
AlexChan <alexander.m.chan@gmail.com>
2015-12-09 09:57:15 -0700

    a basic example of webpack usage

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ import angular from 'angular';
 import uiDate from 'angular-ui-date';
 
 // themes required outside of uiDate to allow customization
-require('jquery-ui/themes/ui-darkness/jquery-ui.min.css');
-// require('jquery-ui/themes/ui-darkness/jquery.ui.datepicker.css');
+require('jquery-ui/themes/base/jquery-ui.min.css');
+// require('jquery-ui/themes/ui-darkness/jquery-ui.min.css');
 
 export default angular.module('MyTest', [uiDate.name])
   .controller('MyCtrl', ['$scope', function($scope) {
